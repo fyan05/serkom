bc43a33c57c585f9674ba637799eed879d9f1001
fyan05 <adesofyanirfani26@gmail.com>
2025-10-06 19:04:34 +0700

    kirim data

--- resources/views/admin/galeri.blade.php
+++ resources/views/admin/galeri.blade.php
@@ -7,16 +7,18 @@
         @foreach($galeris as $galeri)
         <div class="col-md-4 mb-4">
             <div class="card h-100">
-                @if ($galeri->jenis == 'foto')
-                    <img src="{{ asset('storage/galeri'.$galeri->file) }}" alt="" srcset="">
-                @elseif ($galeri->jenis == 'video')
-                    <video width="100%" height="auto" controls>
-                        <source src="{{ asset('storage/galeri'.$galeri->file) }}" type="video/mp4">
-                        Your browser does not support the video tag.
-                    </video>
-                @else
-                    <p>Jenis file tidak dikenali.</p>
-                @endif
+                <div class="d-flex align-items-center justify-content-center" style="height: 250px; background: #f8f9fa;">
+                    @if ($galeri->jenis == 'foto')
+                        <img src="{{ asset('storage/galeri/' . $galeri->file) }}" alt="" class="img-thumbnail" style="max-height: 220px; max-width: 100%; object-fit: contain;">
+                    @elseif ($galeri->jenis == 'video')
+                        <video style="max-height: 220px; max-width: 100%; object-fit: contain;" controls>
+                            <source src="{{ asset('storage/galeri/'. $galeri->file) }}" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    @else
+                        <p>Jenis file tidak dikenali.</p>
+                    @endif
+                </div>
                 <div class="card-body">
                     <h5 class="card-title">{{ $galeri->judul }}</h5>
                     <p class="card-text">{{ $galeri->deskripsi }}</p>
